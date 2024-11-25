134cc9689f6e23266745f638fa7b8e872595edca
Andreas <andreas.martin@andreasmartin.ch>
2024-11-25 08:29:11 +0000

    Fix

--- setup.py
+++ setup.py
@@ -8,13 +8,11 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/MartinLabScience/MartinLab-Utils",
-    author='Andreas Martin',
-    license='GPLv3',
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
