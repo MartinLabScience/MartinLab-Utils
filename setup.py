668c0a95ab88f2bfe94b29410d885a83e783e7f3
Andreas <andreas.martin@andreasmartin.ch>
2024-11-25 09:04:16 +0000

    Fix

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='martinlab-utils',
+    name="martinlab-utils",
     use_scm_version=True,
-    setup_requires=['setuptools-scm'],
-    description='Code and utilities used by Martin Lab members for teaching and research.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/MartinLabScience/MartinLab-Utils',
-    author='Andreas Martin',
-    license='GPLv3',
-    packages=find_packages(),
+    setup_requires=["setuptools_scm"],
+    description="Code and utilities used by Martin Lab members for teaching and research.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/MartinLabScience/MartinLab-Utils",
+    author="Andreas Martin",
+    license="GPLv3",
+    packages=find_packages(include=["martinlab", "martinlab.*"]),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
